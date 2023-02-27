b97e9203be9f0630ed1e80e91cc13727f5b5af23
Yana <yadivenko@gmail.com>
2023-02-27 16:20:12 +0300

    2

--- Itog/Program.cs
+++ Itog/Program.cs
@@ -2,3 +2,15 @@
 string[] arr2 = new string[arr1.Length];
 
 
+void SecondArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
